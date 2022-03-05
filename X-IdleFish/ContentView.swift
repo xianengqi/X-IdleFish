0b9c644556f74ae61f24d9166213a01a177fd101
XiaNengQi <920986617@qq.com>
2022-03-05 21:36:53 +0800

    Initial Commit

---
+++ X-IdleFish/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  X-IdleFish
+//
+//  Created by 夏能啟 on 3/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ X-IdleFish/X_IdleFishApp.swift
@@ -0,0 +1,17 @@
+//
+//  X_IdleFishApp.swift
+//  X-IdleFish
+//
+//  Created by 夏能啟 on 3/5/22.
+//
+
+import SwiftUI
+
+@main
+struct X_IdleFishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
